46ba03726a2628d2da6549a8ffc4153a01fec4d3
dcavadiam <diegocamodev@gmail.com>
2026-03-02 01:58:26 -0500

    Add metadata for SEO optimization in layout.tsx, including Open Graph and Twitter card details for enhanced social sharing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,29 @@ import Footer from "@/components/layout/Footer";
 export const metadata: Metadata = {
   title: "Cursos de Psicología con Certificado en 2026 | Adipa Chile",
   description: "Mira los mejores cursos de psicología para realizar en el 2026. Recibe certificado oficial al finalizar. ¡Conoce Adipa Chile!",
+  robots: "index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1",
+  openGraph: {
+    url: "https://adipa.cl/cursos/",
+    siteName: "ADIPA Chile",
+    locale: "es_CL",
+    type: "article",
+    title: "Cursos de Psicología con Certificado en 2026 | Adipa Chile",
+    description: "Mira los mejores cursos de psicología para realizar en el 2026. Recibe certificado oficial al finalizar. ¡Conoce Adipa Chile!",
+    images: [
+      {
+        url: "https://adipa.cl/content/uploads/2025/01/cursos-banner-2025-2.webp",
+        width: 992,
+        height: 463,
+        alt: "cursos banner 2025",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: "Cursos de Psicología con Certificado en 2026 | Adipa Chile",
+    description: "Mira los mejores cursos de psicología para realizar en el 2026. Recibe certificado oficial al finalizar. ¡Conoce Adipa Chile!",
+    images: ["https://adipa.cl/content/uploads/2025/01/cursos-banner-2025-2.webp"],
+  },
 };
 
 export default function RootLayout({
